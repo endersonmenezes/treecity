0b05a951b2701d3a148e09bdcba95a29cf425e36
Enderson Menezes <enderson_mc@hotmail.com>
2018-10-20 16:57:00 -0300

    correção dos hosts

--- treecity/settings.py
+++ treecity/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'citytree.herokuapp.com', 'treecity.ml']
+ALLOWED_HOSTS = ['localhost', 'citytree.herokuapp.com', 'treecity.ml', 'www.treecity.ml']
 
 
 # Application definition
