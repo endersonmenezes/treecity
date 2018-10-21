355d159e104c7e2022493190e04333517d5dbb9c
Enderson Menezes <enderson_mc@hotmail.com>
2018-10-21 07:09:59 -0300

    corrigindo área da prefeitura

--- app_prefeitura/apps.py
+++ app_prefeitura/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppPrefeituraConfig(AppConfig):
     name = 'app_prefeitura'
-    verbose_name = '3 - Área de Administração da Empresa'
+    verbose_name = '3 - Área de Administração da Prefeitura'
